ff013ad2aa89641f5ac600bba2b63edb2507e531
peefer66 <peefer1966@gmail.com>
2018-05-10 15:56:49 +0000

    start of distribution plots

---
+++ Examples/distplot.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import numpy as np 
+import plotly.offline as pyo 
+import plotly.figure_factory as ff 
+
+x = np.random.randn(100)
--- Examples/linecharts.py
+++ Examples/linecharts.py
@@ -4,6 +4,7 @@ import pandas as pd
 import plotly.graph_objs as go
 import plotly.offline as pyo
 
+
 #create random seed
 np.random.seed(56)
 # x values - create numbers evenly spaced between 0 and 1
